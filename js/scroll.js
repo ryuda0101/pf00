54719abaf0f20cb58b74f93ba6a7e1ae5231c88d
ryuda <ryuda0101@naver.com>
2022-12-06 12:28:22 +0900

    포트폴리오 소개페이지

--- js/scroll.js
+++ js/scroll.js
@@ -1,41 +1,74 @@
+// 변수 세팅
+// 배경에 움직이는 글자
 const main_bg = document.querySelector(".main_banner .center .bg");
 const contact_bg = document.querySelector(".contact .center .bg");
-const all = document.querySelector("*");
+// 스크롤 이동 될 태그들
+let container = document.querySelectorAll("#wrap .container .cont");
+// 헤더의 gnb 메뉴
+const gnb_menu = document.querySelectorAll("#wrap .header .center ul li");
 
-const main_banner = document.querySelector(".main_banner").offsetTop;
-const artwork = document.querySelectorAll(".artworks .artwork");
-const artwork01 = document.querySelector(".artworks .artwork01");
-const artwork02 = document.querySelector(".artworks .artwork02");
-const artwork03 = document.querySelector(".artworks .artwork03");
-const contact = document.querySelector(".contact").offsetTop;
+// gnb 메뉴 클릭시 해당 태그로 이동 이벤트
+gnb_menu.forEach((item,index) => {
+    item.addEventListener("click",(event) => {
+        event.preventDefault();
+        window.scrollTo({
+            top:container[index].offsetTop + 1,
+            behavior:"smooth"
+        });
+    });
+});
 
+// 스크롤 이벤트시 해당 태그 디자인 변화
 window.addEventListener("scroll",() => {
     scTop = window.scrollY;
     if(scTop > 0){
         main_bg.style.left = scTop + "px";
+        header_remove();
     }
-    if(scTop > artwork01.offsetTop) {
-        artwork.forEach((item, index) => {
-            item.classList.add("black");
-        });
-    }
-    if(scTop > artwork02.offsetTop) {
-        artwork.forEach((item, index) => {
-            item.classList.remove("black");
-        });
-    }
-    if(scTop > artwork03.offsetTop) {
-        artwork.forEach((item, index) => {
-            item.classList.add("black");
-        });
-        if(window.matchMedia('screen and (min-width:1201px)').matches) {
-            contact_bg.style.left = (scTop - artwork03.offsetTop - 3000) + "px";
-        }
-        else if(window.matchMedia('screen and (max-width:1200px) and (min-width:1023px)').matches) {
-            contact_bg.style.left = (scTop - artwork03.offsetTop - 2000) + "px";
-        }
-        else if (window.matchMedia('screen and (max-width:1023px) and (min-width:768px)').matches) {
-            contact_bg.style.left = (scTop - artwork03.offsetTop - 1000) + "px";
+    for(let i = 0; i < container.length; i++){
+        if (scTop > container[i].offsetTop) {
+            header_remove();
+            gnb_menu[i].classList.add("black");
+            if (i == 0 || i == 2) {
+                addBlack();
+            }
+            else if (scTop > container[2].offsetTop) {
+                if(window.matchMedia('screen and (min-width:1201px)').matches) {
+                    bg_left(3000);
+                }
+                else if(window.matchMedia('screen and (max-width:1200px) and (min-width:1023px)').matches) {
+                    bg_left(2000);
+                }
+                else if (window.matchMedia('screen and (max-width:1023px) and (min-width:768px)').matches) {
+                    bg_left(1000);
+                }
+            }
+            else {
+                removeBlack();
+            }
         }
     }
 });
+
+// 함수 세팅
+function addBlack (){
+    container.forEach((item, index) => {
+        item.classList.add("black");
+    });
+}
+
+function removeBlack () {
+    container.forEach((item, index) => {
+        item.classList.remove("black");
+    });
+}
+
+function bg_left (minus) {
+    contact_bg.style.left = (scTop - container[2].offsetTop - minus) + "px";
+}
+
+function header_remove () {
+    gnb_menu.forEach((item,index) => {
+        item.classList.remove("black");
+    });
+}
